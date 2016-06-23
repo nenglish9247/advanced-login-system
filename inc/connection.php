c54090c136ca77ad05ca1fb83d51943e025d1db7
nenglish9247 <nenglish998@gmail.com>
2016-06-22 23:10:15 -0400

    Update connection.php

--- inc/connection.php
+++ inc/connection.php
@@ -2,7 +2,12 @@
 class connection extends PDO {
   private static $instance;
   public function __construct($DB_TYPE, $DB_HOST, $DB_NAME, $DB_USER, $DB_PASS) {
-    
+    try {
+      parent::__construct($DB_TYPE.':host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8', $DB_USER, $DB_PASS);
+      $this->exec('SET CHARACTER SET utf8');
+    } catch (PDOException $e) {
+      die('Connection failed: ' . $e->getMessage());
+    }
   }
 }
 ?>
